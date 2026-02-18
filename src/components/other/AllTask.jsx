b4fd8a9f9fdcba7584afd2f417a490045d07802b
shivansh6602 <shivanshdixit2002@gmail.com>
2026-02-18 23:00:01 +0530

    feat: integrate AuthContext into AllTask component for global state access

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
 
-
+const authData = useContext(AuthContext)  
    
   return (
     <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
@@ -13,16 +14,16 @@ const AllTask = () => {
             <h5 className='text-lg font-medium w-1/5'>Completed</h5>
             <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
-        <div className=''>
-        
+        <div className='h-[80%] overflow-auto'>
+        {authData.employees.map(function(elem){
             return <div  className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='text-lg font-medium  w-1/5'></h2>
-            <h3 className='text-lg font-medium w-1/5 text-blue-400'></h3>
-            <h5 className='text-lg font-medium w-1/5 text-yellow-400'></h5>
-            <h5 className='text-lg font-medium w-1/5 text-white'></h5>
-            <h5 className='text-lg font-medium w-1/5 text-red-600'></h5>
+            <h2 className='text-lg font-medium  w-1/5'>{elem.name}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCounts.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
         </div>
-        
+        })}
         </div>
         
         
--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -4,11 +4,16 @@ import { setLocalStorage } from '../../utils/localStorage'
 const Header = ({data}) => {
 
 
+const logOutUser = () => {
+   localStorage.setItem('loggedInUser', '')
+   window.location.reload()
+}
+
   
   return (
     <div className='flex items-end justify-between'>
-        <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{data.name} 👋</span></h1>
-        <button className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+        {/* <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>{data.username} 👋</span></h1> */}
+        <button onClick={logOutUser} className='bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
