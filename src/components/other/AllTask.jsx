495bf1c7aaf47dcefa7c71f165513bb6364f1415
shivansh6602 <shivanshdixit2002@gmail.com>
2026-02-19 18:00:43 +0530

    fix: resolve dashboard rendering issue and correct AuthContext integration

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -3,7 +3,8 @@ import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
 
-const authData = useContext(AuthContext)  
+   const [userData,setUserData] =  useContext(AuthContext)
+
    
   return (
     <div className='bg-[#1c1c1c] p-5 rounded mt-5'>
@@ -14,9 +15,9 @@ const authData = useContext(AuthContext)
             <h5 className='text-lg font-medium w-1/5'>Completed</h5>
             <h5 className='text-lg font-medium w-1/5'>Failed</h5>
         </div>
-        <div className='h-[80%] overflow-auto'>
-        {authData.employees.map(function(elem){
-            return <div  className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+        <div className=''>
+        {userData.map(function(elem,idx){
+            return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
             <h2 className='text-lg font-medium  w-1/5'>{elem.name}</h2>
             <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
             <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,29 +1,29 @@
-import React, { createContext, useState, useEffect } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { getLocalStorage, setLocalStorage } from '../utils/localStorage'
 
 export const AuthContext = createContext()
 
+const AuthProvider = ({ children }) => {
+    // localStorage.clear()
 
-const AuthProvider = ({children}) => {
+    
+const [userData, setUserData] = useState([])
 
+    useEffect(() => {
+        setLocalStorage()
+        const {employees} = getLocalStorage()
+        setUserData(employees)
+    }, [])
+    
+    
 
-const [userData, setUserData] = useState(null)
-
-useEffect(() => {
-setLocalStorage()
-  const {employees, admin} = getLocalStorage()
-setUserData({employees,admin})
-
-}, [])
-
-
-
-
-  return (
-    <div><AuthContext.Provider value={userData}>
-        {children}
-        </AuthContext.Provider></div>
-  )
+    return (
+        <div>
+            <AuthContext.Provider value={[userData,setUserData]}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    )
 }
 
 export default AuthProvider
